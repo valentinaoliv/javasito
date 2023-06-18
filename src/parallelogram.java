11c956193a5b8915e64a691a55f6c59b9ba0476c
valentinaoliv <danna2211334@correo.edu.co>
2023-06-18 12:04:16 -0500

    Java Static Initializer Block

--- src/parallelogram.java
+++ src/parallelogram.java
@@ -1,20 +1,23 @@
 import java.util.*;
+
 public class parallelogram {
+
     public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int b = sc.nextInt();
+            int h = sc.nextInt();
 
-        Scanner sc = new Scanner(System.in);
-        int b = sc.nextInt();
-        int h =sc.nextInt();
-
-        if(b>0 && h>0){
-            if(b <= 100 && h <= 100){
-                int area=b*h;
-                System.out.println(area);
+            if (b > 0 && h > 0) {
+                if (b <= 100 && h <= 100) {
+                    int area = b * h;
+                    System.out.println(area);
+                }
+            } else {
+                throw new Exception("java.lang.Exception: Breadth and height must be positive");
             }
-        }else{
-            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-        sc.close();
     }
     }
-}
+
